7b84bad2ef7255013168defcb8c5feb2c4c21b7b
AventureCloud <93791265+AventureCloud@users.noreply.github.com>
2022-01-16 09:13:03 -0500

    Update Install-Full-IIS.ps1

--- Install-Full-IIS.ps1
+++ Install-Full-IIS.ps1
@@ -1,4 +1,6 @@
-# This script installs most IIS features
+#raghavendra@aventure.cloud
+# This script installs all IIS features
+# To see the list of available IIS features execute: Get-WindowsFeature web-*
 
 Set-ExecutionPolicy UnRestricted -force
 Get-WindowsFeature -Name Web-* | Install-WindowsFeature
