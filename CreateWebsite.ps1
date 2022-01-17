3ab88d9ef65d03fbb996ab13c1aebc934df617d9
AventureCloud <93791265+AventureCloud@users.noreply.github.com>
2022-01-17 00:55:37 -0500

    Update CreateWebsite.ps1

--- CreateWebsite.ps1
+++ CreateWebsite.ps1
@@ -1,5 +1,6 @@
 Set-ExecutionPolicy UnRestricted -force
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
+Get-WindowsFeature -Name Web-* | Install-WindowsFeature
 $source = 'https://github.com/AventureCloud/VirtualMachines/raw/main/SampleWebsite/speed.zip' 
 $destination = 'C:\inetpub\wwwroot\speed.zip'
 Invoke-WebRequest -Uri $source -OutFile $destination
