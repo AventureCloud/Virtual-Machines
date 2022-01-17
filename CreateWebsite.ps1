7fcf1aeb549242df6e52caad2fbf0589d12c6f97
AventureCloud <93791265+AventureCloud@users.noreply.github.com>
2022-01-17 00:52:05 -0500

    Update CreateWebsite.ps1

--- CreateWebsite.ps1
+++ CreateWebsite.ps1
@@ -1,13 +1,10 @@
-#Install a website copying web files from my github
-#Raghavendra A.G
-
-Set-ExecutionPolicy UnRestricted -Force
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Set-ExecutionPolicy UnRestricted -force
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
 $source = 'https://github.com/AventureCloud/VirtualMachines/raw/main/SampleWebsite/speed.zip' 
 $destination = 'C:\inetpub\wwwroot\speed.zip'
 Invoke-WebRequest -Uri $source -OutFile $destination
 Expand-Archive -LiteralPath C:\inetpub\wwwroot\speed.zip -DestinationPath C:\inetpub\wwwroot
 cp C:\inetpub\wwwroot\speed\* C:\inetpub\wwwroot\
-Remove-Item C:\inetpub\wwwroot\speed
+Remove-Item C:\inetpub\wwwroot\speed -Recurse
 
 
